06a59d31cf3ccb8035d3061e227639f623257adf
michaelifo <61135230+michaelifo@users.noreply.github.com>
2020-02-17 07:55:18 +0100

    Add files via upload

--- lokisalle/footer.php
+++ lokisalle/footer.php
@@ -1,19 +1,15 @@
-        
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent2">
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="mentionsLegales.php">Mentions légales <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="cgv.php">Conditions générales <span class="sr-only">(current)</span></a>
-                    </li>
-                </ul>
-            </div>
-        </nav>       
+    
+
+                    <ul class="py-2 my-3 bg-dark navbar-nav mx-auto text-center">
+                        <li class="nav-item">
+                            <a class="nav-link" href="mentionsLegales.php">Mentions légales <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="cgv.php">Conditions générales <span class="sr-only">(current)</span></a>
+                        </li>
+                    </ul>   
+          
+                      
 
         <script src="js/script.js"></script>
         <script src="vendor/jquery/jquery.min.js"></script>
